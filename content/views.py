34434774f7b4636a81153d22f74a5c2a264cb9fb
yih789 <yih789@naver.com>
2022-08-22 19:34:37 +0900

    content Swagger Complete

--- content/views.py
+++ content/views.py
@@ -29,8 +29,22 @@ from .permissions import ContentPermission, CommentPermission
 
 # Content CREATE
 class ContentCreateAPI(CommonView):
+    permission_classes = [ContentPermission]
+
+    @swagger_auto_schema(tags=["게시글 생성"],
+                         request_body=ContentCreateSerializer,
+                         responses={
+                             200: ContentCreateSerializer,
+                             400: '입력값 유효성 검증 실패',
+                             403: '인증 에러',
+                             500: '서버 에러'
+                         },
+                         manual_parameters=[
+                             openapi.Parameter('Authorization', openapi.IN_HEADER, description="AUTH",
+                                               type=openapi.TYPE_STRING)]
+                         )
     def post(self, request):  # CREATE
-        user = get_object_or_404(User, pk=self.user_id)
+        user = get_object_or_404(User, pk=request.user)
         serializer = ContentCreateSerializer(data=request.data)  # request
         if serializer.is_valid():
             serializer.save(writer_id=user)
@@ -42,21 +56,47 @@ class ContentCreateAPI(CommonView):
 class ContentListAPI(APIView, PaginationHandlerMixin):
     pagination_class = ContentsPagination
     serializer_class = ContentSerializer
+
+    @swagger_auto_schema(tags=["게시글 목록"],
+                         request_body=None,
+                         responses={
+                             200: ContentCreateSerializer,
+                             400: '입력값 유효성 검증 실패',
+                             403: '인증 에러',
+                             500: '서버 에러'
+                         })
     # READ list
     def get(self, request, format=None, *args, **kwargs):
         instance = Content.objects.all()
         page = self.paginate_queryset(instance)
         if page is not None:
-            serializer = self.get_paginated_response(self.serializer_class(page,
-                                                                           many=True).data)
+            serializer = self.get_paginated_response(self.serializer_class(page, many=True).data)
         else:
             serializer = self.serializer_class(instance, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-
+    word_field = openapi.Schema(
+        description='검색 키워드',
+        type=openapi.TYPE_STRING,
+    )
+    word_request = openapi.Schema(
+        title='request',
+        type=openapi.TYPE_OBJECT,
+        properties={
+            'word': word_field
+        }
+    )
+    @swagger_auto_schema(tags=["게시글 필터 목록"],
+                         request_body=word_request,
+                         responses={
+                             200: ContentCreateSerializer,
+                             400: '입력값 유효성 검증 실패',
+                             403: '인증 에러',
+                             500: '서버 에러'
+                         })
     # READ filter_list
     def post(self, request, format=None, *args, **kwargs):
-        word = request.POST['word']
+        word = request.data['word']
 
         instance = Content.objects.filter(title__icontains=word) | Content.objects.filter(
             text__icontains=word) | Content.objects.filter(writer_id__username__icontains=word)
@@ -74,37 +114,86 @@ class ContentListAPI(APIView, PaginationHandlerMixin):
 class ContentAPI(APIView):
     permission_classes = [ContentPermission]
 
+    def get_object(self, content_id):
+        content = get_object_or_404(Content, pk=content_id)
+        self.check_object_permissions(self.request, content)
+        return content
 
+    @swagger_auto_schema(tags=["하나의 게시글 조회"],
+                         request_body=None,
+                         responses={
+                             200: ContentCreateSerializer,
+                             400: '입력값 유효성 검증 실패',
+                             403: '인증 에러',
+                             500: '서버 에러'
+                         })
     # READ RETRIEVE
     def get(self, request, content_id):
-
-        content = get_object_or_404(Content, id=content_id)
+        content = self.get_object(content_id)
         serializer = ContentSerializer(content)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
+    @swagger_auto_schema(tags=["하나의 게시글 수정"],
+                         request_body=ContentCreateSerializer,
+                         responses={
+                             200: ContentCreateSerializer,
+                             400: '입력값 유효성 검증 실패',
+                             403: '인증 에러',
+                             500: '서버 에러'
+                         },
+                         manual_parameters=[
+                             openapi.Parameter('Authorization', openapi.IN_HEADER, description="AUTH",
+                                               type=openapi.TYPE_STRING)]
+                         )
     # UPDATE
     def put(self, request, content_id):
-        content = get_object_or_404(Content, id=content_id)
+        content = self.get_object(content_id)
         serializer = ContentCreateSerializer(content, data=request.data)
         if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    @swagger_auto_schema(tags=["하나의 게시글 삭제"],
+                         request_body=None,
+                         responses={
+                             200: 'success delete',
+                             400: '입력값 유효성 검증 실패',
+                             403: '인증 에러',
+                             500: '서버 에러'
+                         },
+                         manual_parameters=[
+                             openapi.Parameter('Authorization', openapi.IN_HEADER, description="AUTH",
+                                               type=openapi.TYPE_STRING)]
+                         )
     # DELETE
     def delete(self, request, content_id):
-        content = get_object_or_404(Content, id=content_id)
+        content = self.get_object(content_id)
         content.delete()  # 삭제
         return Response({"massage": "success delete"}, status=status.HTTP_200_OK)
 
 
 # Comment: pk 필요 x
 class CommentsAPI(CommonView):
+    permission_classes = [CommentPermission]
+
+    @swagger_auto_schema(tags=["댓글 생성"],
+                         request_body=CommentCreateSerializer,
+                         responses={
+                             200: CommentCreateSerializer,
+                             400: '입력값 유효성 검증 실패',
+                             403: '인증 에러',
+                             500: '서버 에러'
+                         },
+                         manual_parameters=[
+                             openapi.Parameter('Authorization', openapi.IN_HEADER, description="AUTH",
+                                               type=openapi.TYPE_STRING)]
+                         )
     # CREATE
     def post(self, request):  # CREATE
         serializer = CommentCreateSerializer(data=request.data)
         if serializer.is_valid():
-            user = get_object_or_404(User, pk=self.user_id)
+            user = get_object_or_404(User, pk=request.user)
             serializer.save(commenter_id=user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response({"massage": "error"}, status=status.HTTP_400_BAD_REQUEST)
@@ -112,16 +201,35 @@ class CommentsAPI(CommonView):
 # Comment: pk 필요
 class CommentAPI(APIView):
     permission_classes = [CommentPermission]
-    '''
-    def get_queryset(self):
-        # all(), 모든 데이터를 갖고 오겠다
-        return Comment.objects.all()
-    '''
+
     def get_object(self, comment_id):
         comment = get_object_or_404(Comment, pk=comment_id)
         self.check_object_permissions(self.request, comment)
         return comment
 
+    text_field = openapi.Schema(
+        description='검색 키워드',
+        type=openapi.TYPE_STRING,
+    )
+    text_request = openapi.Schema(
+        title='request',
+        type=openapi.TYPE_OBJECT,
+        properties={
+            'text': text_field
+        }
+    )
+    @swagger_auto_schema(tags=["댓글 수정"],
+                         request_body=text_request,
+                         responses={
+                             200: CommentCreateSerializer,
+                             400: '입력값 유효성 검증 실패',
+                             403: '인증 에러',
+                             500: '서버 에러'
+                         },
+                         manual_parameters=[
+                             openapi.Parameter('Authorization', openapi.IN_HEADER, description="AUTH",
+                                               type=openapi.TYPE_STRING)]
+                         )
     # UPDATE
     def put(self, request, comment_id):
         comment = self.get_object(comment_id)
@@ -131,6 +239,18 @@ class CommentAPI(APIView):
 
         return Response(serializer.data, status=status.HTTP_200_OK)
 
+    @swagger_auto_schema(tags=["댓글 삭제"],
+                         request_body=None,
+                         responses={
+                             200: 'success delete',
+                             400: '입력값 유효성 검증 실패',
+                             403: '인증 에러',
+                             500: '서버 에러'
+                         },
+                         manual_parameters=[
+                             openapi.Parameter('Authorization', openapi.IN_HEADER, description="AUTH",
+                                               type=openapi.TYPE_STRING)]
+                         )
     # DELETE
     def delete(self, request, comment_id):
         comment = self.get_object(comment_id)
---
+++ login/permissions.py
@@ -0,0 +1,10 @@
+from rest_framework import permissions
+
+class UserPermission(permissions.BasePermission):
+    #글 조회: 누구나, 생성: 로그인 유저, 편집: 글 작성자
+    def has_permission(self, request, view): #객체 전체 권한
+        # list Read: 누구나
+        if request.method == 'GET':
+            return True
+        # obj create: 로그인 유저
+        return request.user.is_authenticated
